b4dc36bf725981d9abd1daf3c9b82faeea55bcd2
Management Trainee Technology Head Office (Selfhy) <095048@bfi.co.id>
2022-06-22 19:38:04 +0700

    add swagger demo springboot

---
+++ DemoSpringHello_Selfhy/src/main/java/com/example/demo/DemoSpringHelloSelfhyApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringHelloSelfhyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringHelloSelfhyApplication.class, args);
+	}
+
+}
