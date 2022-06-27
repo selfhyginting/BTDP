1a69c9f48d43184daace131c1102242a8e6f74d7
Management Trainee Technology Head Office (Selfhy) <095048@bfi.co.id>
2022-06-27 18:08:32 +0700

    add sesi-21

---
+++ sesi-21/SpringFirebaseFCM/src/test/java/com/belajar/spring/SpringFirebaseFcmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.belajar.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringFirebaseFcmApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
